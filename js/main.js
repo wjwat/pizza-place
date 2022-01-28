7fc11d5925bb0231ffad4ae724ae5dd86657873e
Will W <wjwat@onionslice.org>
2022-01-28 11:36:58 -0600

    Create object with passed in values

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
+function Pizza(size, sauce, cheese, toppings) {
+  this.size = size;
+  this.sauce = sauce;
+  this.cheese = cheese;
+  this.toppings = toppings;
+}
+
+// UI
+function createPizzaSelection() {
+  // create them bad boys!
+}
+
+function attachListeners() {
+  // attack them bad boys
+}
+
 $(document).ready(function() {
+  createPizzaSelection();
+  attachListeners();
 });
