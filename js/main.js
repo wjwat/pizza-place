ad01a413f9af5ee42e3c5cd6f10aff194f01de32
Will W <wjwat@onionslice.org>
2022-01-28 11:44:41 -0600

    Create enumerations for use in our Pizza object

--- js/main.js
+++ js/main.js
@@ -1,18 +1,45 @@
+const Sizes = {
+  small: .75,
+  medium: 1,
+  large: 1.25
+};
+const Sauces = {
+  tomato: 0,
+  pesto: 1.25,
+  bbq: 1,
+  'olive oil': 1.25
+};
+const Cheeses = {
+  blend: 0,
+  double: 2,
+  vegan: 1
+};
+const Toppings = {
+  pepperoni: 1,
+  sausage: 1,
+  'bell pepper': .5,
+  'red onion': .5,
+  'black olives': .5,
+  'spicy peppers': .5,
+  pickles: 10,
+  pineapple: 15
+};
+
 function Pizza(size, sauce, cheese, toppings) {
   this.size = size;
   this.sauce = sauce;
   this.cheese = cheese;
   this.toppings = toppings;
-}
+};
 
 // UI
 function createPizzaSelection() {
   // create them bad boys!
-}
+};
 
 function attachListeners() {
   // attack them bad boys
-}
+};
 
 $(document).ready(function() {
   createPizzaSelection();
